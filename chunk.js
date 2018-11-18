06e0199e1f75950dcfcacd9c7c7ee58328b91099
Angela Jin <angelacqjin@gmail.com>
2018-11-17 21:44:36 -0500

    chunk construction

---
+++ chunk.js
@@ -0,0 +1,91 @@
+
+class Chunk {
+    constructor(n, data) {
+        // Chunk dimension (n x n x n blocks)
+        this.n = n;
+
+        // Chunk layout
+        this.data = data;
+    }
+
+    // Create a single vertex and push it onto [vertices] and [indices] lists
+    createVertex(vertices, indices, vertex, u, v) {
+        indices.push(Math.floor(vertices.length / 5));
+        for (var i = 0; i < vertex.length; i++) {
+            vertices.push(vertex[i]);
+        }
+        vertices.push(u);
+        vertices.push(v);
+    }
+
+    // Create a single square from four corners [bl], [br], [tl], and [tr]
+    createSquare(vertices, indices, bl, br, tl, tr) {
+
+        // 3 ---- 2
+        //   |x/|
+        // 1 |/_|
+        this.createVertex(vertices, indices, bl, 0.0, 0.0);
+        this.createVertex(vertices, indices, tr, 1.0, 1.0);
+        this.createVertex(vertices, indices, tl, 0.0, 1.0);
+
+        //   ---- 3
+        //   | /|
+        // 1 |/x| 2
+        this.createVertex(vertices, indices, bl, 0.0, 0.0);
+        this.createVertex(vertices, indices, br, 1.0, 0.0);
+        this.createVertex(vertices, indices, tr, 1.0, 1.0);
+    }
+
+    // Create a vertical wall from bottom two points [x1, y1] and [x2, y2]
+    createWall(vertices, indices, x1, y1, x2, y2, z) {
+        const bl = [x1, z, -y1];
+        const br = [x2, z, -y2];
+        const tl = [x1, z+1, -y1];
+        const tr = [x2, z+1, -y2];
+        this.createSquare(vertices, indices, bl, br, tl, tr);
+    }
+
+    // Create a horizontal tile from two corners [x1, y1] and [x2, y2]
+    createFloor(vertices, indices, x1, y1, x2, y2, z) {
+        const bl = [x1, z, -y1];
+        const br = [x2, z, -y1];
+        const tl = [x1, z, -y2];
+        const tr = [x2, z, -y2];
+        this.createSquare(vertices, indices, bl, br, tl, tr);
+    }
+
+    createBlock(vertices, indices, x, y, z) { // add z
+        this.createWall(vertices, indices, x, y, x + 1, y);
+        this.createWall(vertices, indices, x, y, x, y + 1);
+        this.createWall(vertices, indices, x + 1, y, x + 1, y + 1);
+        this.createWall(vertices, indices, x, y + 1, x + 1, y + 1);
+        this.createFloor(vertices, indices, x, y, x+1, y+1, z);
+        this.createFloor(vertices, indices, x, y, x+1, y+1, z+1);
+    }
+    // Generate the mesh representing the chunk
+    chunkMesh() {
+        const vertices = [];
+        const indices = [];
+        var n = this.n;
+        // .
+        // . x x o
+        // . o o o
+        // | o x o
+        // | _ _ ...
+
+        for (var i = 0; i < data.length; i++) {
+            if(data[i] === 1) {
+                z = Math.floor(i/n);
+                y = Math.floor(i/(Math.pow(n, 2)));
+                x = i - n*z - Math.pow(n, 2)*y;
+
+                createBlock(vertices, indices, x, y, z);
+            }
+        }
+        return {
+            vertices: vertices,
+            indices: indices
+        };
+    }
+
+}
---
+++ main.js
@@ -0,0 +1,132 @@
+const vert = `
+    precision highp float;
+
+    attribute vec3 vert_position;
+    attribute vec2 vert_texCoord;
+
+    // uniform mat4 projection;
+    // uniform mat4 frame;
+    // uniform mat4 model;
+
+    varying vec2 geom_texCoord;
+
+    void main() {
+        //gl_Position = projection * frame * model * vec4(vert_position, 1.0);
+        gl_Position = vec4(vert_position, 1.0);
+        geom_texCoord = vert_texCoord;
+    }`;
+
+const frag = `
+    precision highp float;
+    varying vec2 geom_texCoord;
+
+    uniform sampler2D texture;
+
+    void main() {
+        gl_FragColor = texture2D(texture, geom_texCoord);
+    }`;
+
+var queue = new createjs.LoadQueue();
+queue.on("complete",
+    function() {
+        startWebGL(
+            "webglCanvas",
+            vert,
+            frag,
+
+            // Before update loop
+            function(gl, program) {
+
+                // Load attributes for mesh vertices
+                program.vert_position = gl.getAttribLocation(program, "vert_position");
+                program.vert_texCoord = gl.getAttribLocation(program, "vert_texCoord");
+
+                // Load uniforms
+                program.textureLocation = gl.getUniformLocation(program, "texture");
+                // program.projectionLocation = gl.getUniformLocation(program, "projection");
+                // program.frameLocation = gl.getUniformLocation(program, "frame");
+                // program.modelLocation = gl.getUniformLocation(program, "model");
+
+                // Load textures
+                program.blockTexture = createTexture(gl, queue.getResult("block", false));
+
+                // Draw a single mesh
+                program.draw = function(gl, shape) {
+                    gl.bindBuffer(gl.ARRAY_BUFFER, shape.vertexBuffer);
+                    gl.enableVertexAttribArray(program.vert_position);
+                    gl.vertexAttribPointer(program.vert_position, 3, gl.FLOAT, false, 4 * 5, 0);
+                    gl.enableVertexAttribArray(program.vert_texCoord);
+                    gl.vertexAttribPointer(program.vert_texCoord, 2, gl.FLOAT, false, 4 * 5, 4 * 3);
+                    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+                    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, shape.indexBuffer);
+                    gl.drawElements(gl.TRIANGLES, shape.size, gl.UNSIGNED_SHORT, 0);
+                    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+                };
+
+                const chunkMesh = chunk.chunkMesh();
+                program.chunk = createShape(
+                    gl,
+                    chunkMesh.vertices,
+                    chunkMesh.indices
+                );
+            },
+
+            // During update loop
+            function(gl, program) {
+
+                // Sky color
+                gl.clearColor(0.3, 0.7, 1.0, 1.0);
+                gl.enable(gl.DEPTH_TEST);
+                gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+                // Hot reloading of new mazes
+                // if (RELOAD_MAZE) {
+                //     const chunkMesh = chunk.chunkMesh();
+                //     program.chunk = createShape(
+                //         gl,
+                //         chunkMesh.vertices,
+                //         chunkMesh.indices
+                //     );
+
+                //     RELOAD_MAZE = false;
+                // }
+
+                // // Disable user input if game over
+                // GAME_OVER = maze.gameOver();
+
+                // Update entity position and heading
+                // maze.translate();
+                // maze.rotate();
+                // maze.translateGuardians();
+
+                // Update projection, model, and frame matrices
+                // gl.uniformMatrix4fv(program.modelLocation, false, mat4.create());
+                // gl.uniformMatrix4fv(program.projectionLocation, false, getProjMatrix());
+                // gl.uniformMatrix4fv(program.frameLocation, false, getFrameMatrix());
+
+                // Draw walls
+                gl.activeTexture(gl.TEXTURE0);
+                gl.bindTexture(gl.TEXTURE_2D, program.blockTexture);
+                gl.uniform1i(program.textureLocation, 0);
+                program.draw(gl, program.wall);
+
+            }
+        );
+    }, this
+);
+
+queue.loadManifest([
+    {
+        id: "block",
+        src: "wall.jpg"
+    }
+//     {
+//         id: "floor",
+//         src: "data/floor.jpg"
+//     },
+//     {
+//         id: "eye",
+//         src: "data/eye.jpg"
+//     }
+]);
