3f07903f93788a0e88fb490e97af8dbfebb730ab
Newton Ni <nwtnni@gmail.com>
2018-11-18 22:45:57 -0500

    Implement lazy chunk loading

--- model/src/model.rs
+++ model/src/model.rs
@@ -1,7 +1,14 @@
 use std::collections::HashMap;
 
+use constants::CHUNK_SIZE;
 use data::*;
 use generate::Generator;
+
+const AROUND: [(isize, isize); 9] = [
+    (-1, -1), (0, -1), (1, -1),
+    (0, -1), (0, 0), (0, 1),
+    (1, -1), (1, 0), (1, 1),
+];
 
 #[derive(Default)]
 pub struct World<G: Generator> {
@@ -18,8 +25,35 @@ pub struct World<G: Generator> {
 
 impl <G: Generator> World<G> {
 
-    pub fn connect(&mut self, player: usize) {
-        self.positions.insert(player, Position::default());
+    fn load(&mut self, index: Index) {
+        self.chunks.insert(index, self.generator.generate(index));
+    }
+
+    fn load_around(&mut self, position: Position) -> Vec<Index> {
+        let scale = CHUNK_SIZE as f32;
+        let x = (position.0.x / scale).floor() as isize;
+        let z = (position.0.z / scale).floor() as isize;
+        let loaded = AROUND.iter()
+            .map(|(dx, dz)| Index(x + dx, z + dz))
+            .filter(|index| !self.chunks.contains_key(index))
+            .collect::<Vec<_>>();
+        for index in &loaded {
+            self.load(*index);
+        }
+        loaded
+    }
+
+    pub fn get_chunk(&mut self, index: Index) -> Chunk {
+        if !self.chunks.contains_key(&index) {
+            self.load(index); 
+        }
+        self.chunks[&index].clone()
+    }
+
+    pub fn connect(&mut self, player: usize) -> Vec<Index> {
+        let start = Position::default();
+        self.positions.insert(player, start);
+        self.load_around(start)
     }
 
     pub fn disconnect(&mut self, player: usize) {
