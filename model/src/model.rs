ad0e459ee2af7c6c66e6490910d2e6d97aaea781
Newton Ni <nwtnni@gmail.com>
2018-11-18 23:21:47 -0500

    Split up lazy loading for initialization

--- model/src/model.rs
+++ model/src/model.rs
@@ -25,28 +25,35 @@ pub struct World<G: Generator> {
 
 impl <G: Generator> World<G> {
 
-    fn load(&mut self, index: Index) {
-        self.chunks.insert(index, self.generator.generate(index));
+    fn lazy_load(&mut self, index: Index) -> Option<Index> {
+        if !self.chunks.contains_key(&index) {
+            self.chunks.insert(index, self.generator.generate(index));
+            Some(index)
+        } else {
+            None
+        }
     }
 
-    fn load_around(&mut self, position: Position) -> Vec<Index> {
+    fn around(&self, position: Position) -> impl Iterator<Item = Index> {
         let scale = CHUNK_SIZE as f32;
         let x = (position.0.x / scale).floor() as isize;
         let z = (position.0.z / scale).floor() as isize;
-        let loaded = AROUND.iter()
-            .map(|(dx, dz)| Index(x + dx, z + dz))
-            .filter(|index| !self.chunks.contains_key(index))
-            .collect::<Vec<_>>();
-        for index in &loaded {
-            self.load(*index);
-        }
-        loaded
+        AROUND.iter().map(move |(dx, dz)| Index(x + dx, z + dz))
+    }
+
+    pub fn load_around(&mut self, position: Position) -> Vec<Index> {
+        self.around(position)
+            .inspect(|index| { self.lazy_load(*index); })
+            .collect()
+    }
+
+    pub fn lazy_load_around(&mut self, position: Position) -> Vec<Index> {
+        self.around(position)
+            .filter_map(|index| self.lazy_load(index))
+            .collect()
     }
 
     pub fn get_chunk(&mut self, index: Index) -> Chunk {
-        if !self.chunks.contains_key(&index) {
-            self.load(index); 
-        }
         self.chunks[&index].clone()
     }
 
