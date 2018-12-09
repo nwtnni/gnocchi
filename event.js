32d48282cac77d7fdc2b2424f741a42b0ceb5428
Newton Ni <nwtnni@gmail.com>
2018-12-09 03:06:08 -0500

    Use local server for development

--- event.js
+++ event.js
@@ -4,14 +4,14 @@ var CURR_Y;
 $(function() {
 
 // Local testing
-// const protocol = window.location.protocol === 'https:' && 'wss://' || 'ws://';
-// const host = 'localhost:8080/ws';
-// var connection = new WebSocket(protocol + host);
+const protocol = window.location.protocol === 'https:' && 'wss://' || 'ws://';
+const host = 'localhost:8080/ws';
+var connection = new WebSocket(protocol + host);
 
 // Remote server
-const host = "wss://gnocchi-graphics.herokuapp.com/ws";
-var connection = new WebSocket(host);
-console.log(connection);
+// const host = "wss://gnocchi-graphics.herokuapp.com/ws";
+// var connection = new WebSocket(host);
+// console.log(connection);
 
 connection.onopen = function() {
     console.log("Connected.");
